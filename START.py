fecc8c8da53edf5dd59aee87701e09693e424732
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-04-05 14:28:48 -0300

    Mudado forma de mapear teclas

--- START.py
+++ START.py
@@ -5,46 +5,32 @@ from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-def getKey(cmd):
-	if cmd == 'L': return 'q'              # L
-	elif cmd == 'R': return 'e'            # R
-	elif cmd == 'ACTUP': return 'i'        # Triangle or X
-	elif cmd == 'ACTRIGHT': return 'l'     # Circle   or A
-	elif cmd == 'ACTLEFT': return 'j'      # Square   or Y
-	elif cmd == 'ACTDOWN': return 'k'      # Cross    or B
-	elif cmd == 'START': return Key.space  # Start
-	elif cmd == 'SELECT': return Key.enter # Select
-	elif cmd == 'UP': return Key.up        # Arrow up
-	elif cmd == 'DOWN': return Key.down    # Arrow down
-	elif cmd == 'LEFT': return Key.left    # Arrow left
-	elif cmd == 'RIGHT': return Key.right  # Arrow right
-	elif cmd == 'JOYUP': return 'w'        # Joystick up
-	elif cmd == 'JOYDOWN': return 's'      # Joystick down
-	elif cmd == 'JOYLEFT': return 'a'      # Joystick left
-	elif cmd == 'JOYRIGHT': return 'd'     # Joystick right
-	elif cmd == 'LOAD': return 'o'         # Load state
-	elif cmd == 'SAVE': return 'p'         # Save state
-	elif cmd == 'PAUSE': return Key.esc    # Pause game
-	else: return cmd
-
 PORT = 5000
 async def server(websocket, path):
 	print('[WEBSOCKET] Connected user')
 	async for msg in websocket:
-		msg = msg.upper()
-		msg = msg.split(' ')
-		msg[1] = msg[1].split(',')
+		try:
+			msg = msg.lower()
+			msg = msg.split(' ')
+			msg[1] = msg[1].split(',')
 
-		for key in msg[1]:
-			if msg[0] == 'R':
-				print('[RELEASE] ' + key)
-				keyboard.release(getKey(key))
-			else:
-				print('[ PRESS ] ' + key)
-				keyboard.press(getKey(key))
+			for keyName in msg[1]:
+				key = keyName
+				if len(keyName) > 1:
+					key = Key[keyName]
+
+				if msg[0] == 'r':
+					keyboard.release(key)
+					print('[RELEASE] ' + keyName)
+				else:
+					keyboard.press(key)
+					print('[ PRESS ] ' + keyName)
+		except:
+			print('[ ERROR ] Invalid data!')
+			pass
 
 start_server = websockets.serve(server, port=PORT)
-ip = socket.gethostbyname(socket.gethostname()) + ':' + str(PORT)
+ip = socket.gethostbyname_ex(socket.gethostname())[-1][-1] + ':' + str(PORT)
 
 print('\n[WEBSOCKET] Server started')
 print('\n# Enter this code on the website:')
