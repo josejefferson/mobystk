cf417bde1d245bd0ce2cc5a7d0fb5b74e71a1616
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-03-07 16:48:59 -0300

    Modificado critério de exibir dica

--- web/js/index.js
+++ web/js/index.js
@@ -3,6 +3,11 @@ const ls = localStorage
 const form = document.forms[0]
 const formEls = form.elements
 
+// Contador de visitas
+let hits = parseInt(ls.getItem('joystick.data.hits.home'))
+if (isNaN(hits)) hits = 0
+ls.setItem('joystick.data.hits.home', ++hits)
+
 // Eventos
 window.addEventListener('load', () => {
 	document.body.classList.remove('preload')
@@ -54,8 +59,8 @@ $athPopupClose.addEventListener('click', () => {
 	ls.setItem('joystick.events.addToHomescreenPopup', true)
 })
 
-if (!ls.getItem('joystick.events.addToHomescreenPopup')) {
-	setTimeout(() => {
+if (!ls.getItem('joystick.events.addToHomescreenPopup') && hits % 3 === 0) {
+	window.addEventListener('load', () => {
 		const $video = document.createElement('video')
 		$video.src = 'video/add-to-homescreen-tutorial.mp4'
 		$video.loop = true
@@ -63,40 +68,40 @@ if (!ls.getItem('joystick.events.addToHomescreenPopup')) {
 		$athPopup.querySelector('.content').appendChild($video)
 		$video.play()
 		$athPopup.classList.add('show')
-	}, 10000)
+	})
 }
 
 // Carregar opções
-formEls.code.value = ls.getItem('joystick.code') || window.location.hostname + ':5000'
-formEls.layout.value = ls.getItem('joystick.layout')
-formEls.player.value = ls.getItem('joystick.player')
-formEls.invertL.checked = ls.getItem('joystick.invertL') === 'true'
-formEls.invertR.checked = ls.getItem('joystick.invertR') === 'true'
-formEls.disJoyXAxis.checked = ls.getItem('joystick.disJoyXAxis') === 'true'
-formEls.disJoyYAxis.checked = ls.getItem('joystick.disJoyYAxis') === 'true'
-formEls.dblClickLoadSave.checked = ls.getItem('joystick.dblClickLoadSave') === 'true'
-formEls.vibrate.value = ls.getItem('joystick.vibrate') || '15'
-formEls.vibrateJoystick.value = ls.getItem('joystick.vibrateJoystick') || '5'
-formEls.vibrationFromGame.checked = !(ls.getItem('joystick.vibrationFromGame') === 'false')
-formEls.vgamepad.checked = ls.getItem('joystick.vgamepad') === 'true'
-formEls.background.value = ls.getItem('joystick.background') || 'rgba(0, 0, 0, 1)'
-formEls.color.value = ls.getItem('joystick.color') || 'rgba(255, 255, 255, 0.53)'
-formEls.border.value = ls.getItem('joystick.border') || 'rgba(255, 255, 255, 0.53)'
-formEls.active.value = ls.getItem('joystick.active') || 'rgba(255, 255, 255, 0.2)'
-formEls.bgImage.value = ls.getItem('joystick.bgImage')
-formEls.bgOpacity.value = ls.getItem('joystick.bgOpacity') || '0.5'
-formEls.bgBlur.value = ls.getItem('joystick.bgBlur') || '0'
-formEls.customCSS.value = ls.getItem('joystick.customCSS')
-formEls.driveSensitivity.value = ls.getItem('joystick.driveSensitivity') || '2'
-formEls.drivePrecision.value = ls.getItem('joystick.drivePrecision') || '1'
-if (ls.getItem('joystick.locked') !== null)
+formEls.code.value = ls.getItem('joystick.options.code') || window.location.hostname + ':5000'
+formEls.layout.value = ls.getItem('joystick.options.layout')
+formEls.player.value = ls.getItem('joystick.options.player')
+formEls.invertL.checked = ls.getItem('joystick.options.invertL') === 'true'
+formEls.invertR.checked = ls.getItem('joystick.options.invertR') === 'true'
+formEls.disJoyXAxis.checked = ls.getItem('joystick.options.disJoyXAxis') === 'true'
+formEls.disJoyYAxis.checked = ls.getItem('joystick.options.disJoyYAxis') === 'true'
+formEls.dblClickLoadSave.checked = ls.getItem('joystick.options.dblClickLoadSave') === 'true'
+formEls.vibrate.value = ls.getItem('joystick.options.vibrate') || '15'
+formEls.vibrateJoystick.value = ls.getItem('joystick.options.vibrateJoystick') || '5'
+formEls.vibrationFromGame.checked = !(ls.getItem('joystick.options.vibrationFromGame') === 'false')
+formEls.vgamepad.checked = ls.getItem('joystick.options.vgamepad') === 'true'
+formEls.background.value = ls.getItem('joystick.options.background') || 'rgba(0, 0, 0, 1)'
+formEls.color.value = ls.getItem('joystick.options.color') || 'rgba(255, 255, 255, 0.53)'
+formEls.border.value = ls.getItem('joystick.options.border') || 'rgba(255, 255, 255, 0.53)'
+formEls.active.value = ls.getItem('joystick.options.active') || 'rgba(255, 255, 255, 0.2)'
+formEls.bgImage.value = ls.getItem('joystick.options.bgImage')
+formEls.bgOpacity.value = ls.getItem('joystick.options.bgOpacity') || '0.5'
+formEls.bgBlur.value = ls.getItem('joystick.options.bgBlur') || '0'
+formEls.customCSS.value = ls.getItem('joystick.options.customCSS')
+formEls.driveSensitivity.value = ls.getItem('joystick.options.driveSensitivity') || '2'
+formEls.drivePrecision.value = ls.getItem('joystick.options.drivePrecision') || '1'
+if (ls.getItem('joystick.options.locked') !== null)
 	document.querySelectorAll('[name=lock]').forEach(e => e.checked = false)
-ls.getItem('joystick.locked')?.split(',')?.forEach(e => {
+ls.getItem('joystick.options.locked')?.split(',')?.forEach(e => {
 	if (e) document.querySelector(`[name=lock][value="${e}"]`).checked = true
 })
-if (ls.getItem('joystick.hidden') !== null)
+if (ls.getItem('joystick.options.hidden') !== null)
 	document.querySelectorAll('[name=hide]').forEach(e => e.checked = false)
-ls.getItem('joystick.hidden')?.split(',')?.forEach(e => {
+ls.getItem('joystick.options.hidden')?.split(',')?.forEach(e => {
 	if (e) document.querySelector(`[name=hide][value="${e}"]`).checked = true
 })
 
@@ -127,31 +132,31 @@ document.forms[0].addEventListener('submit', function (e) {
 		if (e.checked) hiddenItems.push(e.value)
 	})
 
-	localStorage.setItem('joystick.code', elems.code.value)
-	localStorage.setItem('joystick.layout', elems.layout.value)
-	localStorage.setItem('joystick.player', elems.player.value)
-	localStorage.setItem('joystick.debug', elems.debug.checked)
-	localStorage.setItem('joystick.invertL', elems.invertL.checked)
-	localStorage.setItem('joystick.invertR', elems.invertR.checked)
-	localStorage.setItem('joystick.disJoyXAxis', elems.disJoyXAxis.checked)
-	localStorage.setItem('joystick.disJoyYAxis', elems.disJoyYAxis.checked)
-	localStorage.setItem('joystick.dblClickLoadSave', elems.dblClickLoadSave.checked)
-	localStorage.setItem('joystick.vibrate', elems.vibrate.value)
-	localStorage.setItem('joystick.vibrateJoystick', elems.vibrateJoystick.value)
-	localStorage.setItem('joystick.vibrationFromGame', elems.vibrationFromGame.checked)
-	localStorage.setItem('joystick.vgamepad', elems.vgamepad.checked)
-	localStorage.setItem('joystick.background', elems.background.value)
-	localStorage.setItem('joystick.color', elems.color.value)
-	localStorage.setItem('joystick.border', elems.border.value)
-	localStorage.setItem('joystick.active', elems.active.value)
-	localStorage.setItem('joystick.bgImage', elems.bgImage.value)
-	localStorage.setItem('joystick.bgOpacity', elems.bgOpacity.value)
-	localStorage.setItem('joystick.bgBlur', elems.bgBlur.value)
-	localStorage.setItem('joystick.customCSS', elems.customCSS.value)
-	localStorage.setItem('joystick.driveSensitivity', elems.driveSensitivity.value)
-	localStorage.setItem('joystick.drivePrecision', elems.drivePrecision.value)
-	localStorage.setItem('joystick.locked', lockedBtns.join(','))
-	localStorage.setItem('joystick.hidden', hiddenItems.join(','))
+	ls.setItem('joystick.options.code', elems.code.value)
+	ls.setItem('joystick.options.layout', elems.layout.value)
+	ls.setItem('joystick.options.player', elems.player.value)
+	ls.setItem('joystick.options.debug', elems.debug.checked)
+	ls.setItem('joystick.options.invertL', elems.invertL.checked)
+	ls.setItem('joystick.options.invertR', elems.invertR.checked)
+	ls.setItem('joystick.options.disJoyXAxis', elems.disJoyXAxis.checked)
+	ls.setItem('joystick.options.disJoyYAxis', elems.disJoyYAxis.checked)
+	ls.setItem('joystick.options.dblClickLoadSave', elems.dblClickLoadSave.checked)
+	ls.setItem('joystick.options.vibrate', elems.vibrate.value)
+	ls.setItem('joystick.options.vibrateJoystick', elems.vibrateJoystick.value)
+	ls.setItem('joystick.options.vibrationFromGame', elems.vibrationFromGame.checked)
+	ls.setItem('joystick.options.vgamepad', elems.vgamepad.checked)
+	ls.setItem('joystick.options.background', elems.background.value)
+	ls.setItem('joystick.options.color', elems.color.value)
+	ls.setItem('joystick.options.border', elems.border.value)
+	ls.setItem('joystick.options.active', elems.active.value)
+	ls.setItem('joystick.options.bgImage', elems.bgImage.value)
+	ls.setItem('joystick.options.bgOpacity', elems.bgOpacity.value)
+	ls.setItem('joystick.options.bgBlur', elems.bgBlur.value)
+	ls.setItem('joystick.options.customCSS', elems.customCSS.value)
+	ls.setItem('joystick.options.driveSensitivity', elems.driveSensitivity.value)
+	ls.setItem('joystick.options.drivePrecision', elems.drivePrecision.value)
+	ls.setItem('joystick.options.locked', lockedBtns.join(','))
+	ls.setItem('joystick.options.hidden', hiddenItems.join(','))
 
 	loading()
 	location.href = 'joystick.html'
@@ -176,7 +181,7 @@ function createPickr(el, defaultColor, opacity) {
 	return Pickr.create({
 		el: `.pickr-${el}`,
 		theme: 'classic',
-		default: ls['joystick.' + el] || defaultColor,
+		default: ls['joystick.options.' + el] || defaultColor,
 		defaultRepresentation: 'HEXA',
 		comparison: false,
 		autoReposition: true,
